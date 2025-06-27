3f4c6944d5f9b38cc17405b87493a6d3fd8ca0ee
ShipWithKartik <kartikawasthi2022@gmail.com>
2025-06-27 13:31:56 +0530

    React Router

--- mernDiaryFrontend/src/App.jsx
+++ mernDiaryFrontend/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Home from './pages/Home';
+
 
 const App = () => {
 
   return (
 
     <div>
-      App
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/sign-up' element={<SignUp />} />
+
+        </Routes>
+      </BrowserRouter>
+
     </div>
 
   )
   
 }
 
-export default App
+export default App;

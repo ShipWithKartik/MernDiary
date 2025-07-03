7318d2e297ff921a0e49286742a129edf6860822
ShipWithKartik <kartikawasthi2022@gmail.com>
2025-07-03 11:16:28 +0530

    added redux persist

--- mernDiaryFrontend/src/redux/store.js
+++ mernDiaryFrontend/src/redux/store.js
@@ -1,8 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './slice/userSlice'
+import storage from 'redux-persist/lib/storage' 
+import persistReducer from 'redux-persist/es/persistReducer'
+import persistStore from 'redux-persist/es/persistStore'
 
-export default configureStore({
-  reducer: {
-    user: userReducer
-  },
+const rootReducer = combineReducers({
+  user: userReducer
+})  
+
+const persistConfig = {
+  key:'root',
+  storage,
+  version:1
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+
+  middleware:(getDefaultMiddleware) => {
+    return getDefaultMiddleware({serializableCheck: false})
+  }
+
+
 })
+
+
+export const persistor = persistStore(store);

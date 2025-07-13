dd25a3fc810dfa5d8b9bf2cc18ccd28cd3a80372
ShipWithKartik <kartikawasthi2022@gmail.com>
2025-07-13 12:48:23 +0530

    deployment issue

--- mernDiaryBackend/utils/error.js
+++ mernDiaryBackend/utils/error.js
@@ -1,10 +1,13 @@
-export const errorHandler = (statusCode,message)=>{
-
+const errorHandler = (statusCode,message)=>{
     const error = new Error()
     error.statusCode = statusCode;
     error.message = message;
 
     return error;
 }
+
+module.exports = {
+    errorHandler
+}   
 
 // Creates a new Error object , adds custom properties (statusCode and message) and returns the enhanced error object
